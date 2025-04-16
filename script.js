7efd246de136abf677bd9f2fc4c7a16cfcfc4ccf
Shreenivas <s.n.sharma0714@gmail.com>
2025-04-16 23:20:06 +0530

    initial commit

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
 // array
 // foreach map.filter.find.indexof
-var arr = [1, 2, 3, 4, 5];
-arr.forEach(function (val) {
-    console.log(val + " Hello World!");
-})
+// var arr = [1, 2, 2, 3, 4, 5];
+// arr.forEach(function (val) {
+//     console.log(val + " Hello World!");
+// })
 
+// var ans = arr.map(function (val) {
+//     return val * 2;
+// })
+// console.log(ans);
+
+// var ans = arr.filter(function (val) {
+//     if (val > 2) {
+//         return true;
+//     }
+//     else return false;
+// })
+// console.log(ans);
+
+// var ans = arr.find(function (val) {
+//     if (val === 2) {
+//         return true;
+//     }
+//     else return false;
+// })
+// console.log(ans);
+
+var obj = {
+    name : "Harsh",
+    age : 12
+}
+
+Object.freeze(obj);
+obj.age = 25;
