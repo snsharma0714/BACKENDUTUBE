6dc1c3e9d8b42a995a195cd25b63b7292daf814b
Shreenivas <s.n.sharma0714@gmail.com>
2025-04-30 17:06:59 +0530

    initial commit

--- lect6miniproj/index.js
+++ lect6miniproj/index.js
@@ -1,23 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const fs = require('fs');
+const { log } = require('console');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res){
-    res.render("index");
+    fs.readdir(`./files`, function(err, files){
+        res.render("index",{files : files});
+    })
 });
-app.get('/profile/:username', function(req, res){
-    res.send(`welcome to ${req.params.username} profile`);
+app.get('/file/:filename', function(req, res){
+    fs.readFile(`./files/${req.params.filename}`, "utf-8", function(err, filedata){
+        res.render('show', {filename: req.params.filename, filedata: filedata});
+    })
+});
+app.post('/create', function(req, res){
+    fs.writeFile(`./files/${req.body.title.split(' ').join('')}.txt`, req.body.details, function(err){
+        res.redirect("/")
+    });
 });
 
-app.get("/author/:username/:age",function(req, res){
-    res.send(`welcome to ${req.params.username} profile and your age is ${req.params.age}`);
-})
-
-app.listen(3000, function(){
-    console.log("Server is running on port 3000");
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, function(err){
+    if(err) {
+        console.error("Error starting server:", err);
+        return;
+    }
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
